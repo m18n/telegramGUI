6893b3ea209e414dcd2204dbe82538836e23c467
Misha <20xim04@gmail.com>
2022-03-30 13:10:20 +0300

    CreateBlocks

--- assets/js/setka.js
+++ assets/js/setka.js
@@ -6,4 +6,22 @@ function Vp(){
 function Nz(){
     let sc=document.getElementById('sc');
     sc.scrollTop+=220;
+    
+}
+function CreateBlock(count){
+   
+    let my_div = document.getElementById("sc");
+    for(let i=0;i<count;i++){
+        var div=document.createElement("div");
+        div.innerHTML = "<img src=\"img/load.png\" alt=\"\"><span id=\"load\">0%</span>";
+        my_div.appendChild(div);
+    }
+
+}
+function SetPath(idpath){
+    var ts=idpath.split(":");
+    let vdiv=document.getElementById("sc");
+    let id=parseInt(ts[0]);
+    vdiv.children[id].children[0].src=ts[1];
+    
 }
--- main.cpp
+++ main.cpp
@@ -2,32 +2,25 @@
 #include <AppCore/Window.h>
 #include <AppCore/Overlay.h>
 #include <AppCore/JSHelpers.h>
-#include<iostream>
+#include <JavaScriptCore/JSRetainPtr.h>
+#include <iostream>
 using namespace ultralight;
 JSValueRef CNz(JSContextRef ctx, JSObjectRef function,
-                           JSObjectRef thisObject, size_t argumentCount,
-                           const JSValueRef arguments[], JSValueRef *exception)
-  {
-
-    std::cout<<"NZ";
-
-    return JSValueMakeNull(ctx);
-  }
+               JSObjectRef thisObject, size_t argumentCount,
+               const JSValueRef arguments[], JSValueRef *exception);
 
 JSValueRef CVp(JSContextRef ctx, JSObjectRef function,
-                           JSObjectRef thisObject, size_t argumentCount,
-                           const JSValueRef arguments[], JSValueRef *exception)
-  {
-
-    std::cout<<"NZ";
-
-    return JSValueMakeNull(ctx);
-  }
-class UltralightMain : public WindowListener,public LoadListener
+               JSObjectRef thisObject, size_t argumentCount,
+               const JSValueRef arguments[], JSValueRef *exception);
+class UltralightMain : public WindowListener, public LoadListener
 {
   RefPtr<Overlay> overlay;
+  JSContextRef ctx;
 
 public:
+  UltralightMain(){
+
+  }
   UltralightMain(Ref<Window> win)
   {
 
@@ -40,17 +33,27 @@ public:
     overlay->view()->LoadURL("file:///index.html");
     overlay->view()->set_load_listener(this);
   }
+  void SetWindow(Ref<Window> win){
+    overlay = Overlay::Create(win, win->width(), win->height(), 0, 0);
+
+    // Load its content from file
+    // All the files must be put into the assets folder, then you can create other subdirectories and all you need
+
+    overlay->view()->LoadURL("file:///index.html");
+    overlay->view()->set_load_listener(this);
+  }
   virtual void OnResize(uint32_t width, uint32_t height) override
   {
     overlay->Resize(width, height);
   }
-  
-  void RegistrFunctionJs(JSContextRef ctx,std::string namefunction,JSObjectCallAsFunctionCallback fun){
-      JSStringRef name = JSStringCreateWithUTF8CString(namefunction.c_str());
+
+  void RegistrFunctionJs(std::string namefunction, JSObjectCallAsFunctionCallback fun)
+  {
+    JSStringRef name = JSStringCreateWithUTF8CString(namefunction.c_str());
 
     // Create a garbage-collected JavaScript function that is bound to our
     // native C callback 'OnButtonClick()'.'
-    
+
     JSObjectRef func = JSObjectMakeFunctionWithCallback(ctx, name,
                                                         fun);
 
@@ -62,13 +65,114 @@ public:
     JSObjectSetProperty(ctx, globalObj, name, func, 0, 0);
 
     // Release the JavaScript String we created earlier.
-    JSStringRelease(name);  
+    JSStringRelease(name);
   }
-  
+  void CallFunctionJs(std::string namefunction, std::string val)
+  {
+    JSRetainPtr<JSStringRef> str = adopt(
+        JSStringCreateWithUTF8CString(namefunction.c_str()));
+
+    // Evaluate the string "ShowMessage"
+    JSValueRef func = JSEvaluateScript(ctx, str.get(), 0, 0, 0, 0);
+
+    // Check if 'func' is actually an Object and not null
+    if (JSValueIsObject(ctx, func))
+    {
+
+      // Cast 'func' to an Object, will return null if typecast failed.
+      JSObjectRef funcObj = JSValueToObject(ctx, func, 0);
+
+      // Check if 'funcObj' is a Function and not null
+      if (funcObj && JSObjectIsFunction(ctx, funcObj))
+      {
+
+        // Create a JS string from null-terminated UTF8 C-string, store it
+        // in a smart pointer to release it when it goes out of scope.
+        JSRetainPtr<JSStringRef> msg =
+            adopt(JSStringCreateWithUTF8CString(val.c_str()));
+
+        // Create our list of arguments (we only have one)
+        const JSValueRef args[] = {JSValueMakeString(ctx, msg.get())};
+
+        // Count the number of arguments in the array.
+        size_t num_args = sizeof(args) / sizeof(JSValueRef *);
+
+        // Create a place to store an exception, if any
+        JSValueRef exception = 0;
+
+        // Call the ShowMessage() function with our list of arguments.
+        JSValueRef result = JSObjectCallAsFunction(ctx, funcObj, 0,
+                                                   num_args, args,
+                                                   &exception);
+
+        if (exception)
+        {
+          // Handle any exceptions thrown from function here.
+        }
+
+        if (result)
+        {
+          // Handle result (if any) here.
+        }
+      }
+    }
+  }
+  void CallFunctionJs(std::string namefunction, double val)
+  {
+    JSRetainPtr<JSStringRef> str = adopt(
+        JSStringCreateWithUTF8CString(namefunction.c_str()));
+
+    // Evaluate the string "ShowMessage"
+    JSValueRef func = JSEvaluateScript(ctx, str.get(), 0, 0, 0, 0);
+
+    // Check if 'func' is actually an Object and not null
+    if (JSValueIsObject(ctx, func))
+    {
+
+      // Cast 'func' to an Object, will return null if typecast failed.
+      JSObjectRef funcObj = JSValueToObject(ctx, func, 0);
+
+      // Check if 'funcObj' is a Function and not null
+      if (funcObj && JSObjectIsFunction(ctx, funcObj))
+      {
+
+        // Create a JS string from null-terminated UTF8 C-string, store it
+        // in a smart pointer to release it when it goes out of scope.
+       
+        
+        // Create our list of arguments (we only have one)
+        const JSValueRef args[] = {JSValueMakeNumber(ctx,val)};
+
+        // Count the number of arguments in the array.
+        size_t num_args = sizeof(args) / sizeof(JSValueRef *);
+
+        // Create a place to store an exception, if any
+        JSValueRef exception = 0;
+
+        // Call the ShowMessage() function with our list of arguments.
+        JSValueRef result = JSObjectCallAsFunction(ctx, funcObj, 0,
+                                                   num_args, args,
+                                                   &exception);
+
+        if (exception)
+        {
+          // Handle any exceptions thrown from function here.
+        }
+
+        if (result)
+        {
+          // Handle result (if any) here.
+        }
+      }
+    }
+  }
+  void LoadMedia(int id,std::string path){
+    CallFunctionJs("SetPath",std::to_string(id)+":"+path);
+  }
   void OnDOMReady(View *caller,
-                                  uint64_t frame_id,
-                                  bool is_main_frame,
-                                  const String &url) override
+                  uint64_t frame_id,
+                  bool is_main_frame,
+                  const String &url) override
   {
 
     // Acquire the JS execution context for the current page.
@@ -80,16 +184,37 @@ public:
 
     // Get the underlying JSContextRef for use with the
     // JavaScriptCore C API.
-    JSContextRef ctx = context.get();
-    RegistrFunctionJs(ctx,"CNz",CNz);
-    RegistrFunctionJs(ctx,"CVp",CVp);
+    ctx = context.get();
+    RegistrFunctionJs("CNz", CNz);
+    RegistrFunctionJs("CVp", CVp);
+    CallFunctionJs("CreateBlock",20);
+    LoadMedia(0,"img/2.jpg");
     // Create a JavaScript String containing the name of our callback.
-   
   }
   virtual void OnClose() override {}
   virtual ~UltralightMain() {}
 };
+UltralightMain sa;
+JSValueRef CNz(JSContextRef ctx, JSObjectRef function,
+               JSObjectRef thisObject, size_t argumentCount,
+               const JSValueRef arguments[], JSValueRef *exception)
+{
 
+  std::cout << "NZ";
+  
+  
+  return JSValueMakeNull(ctx);
+}
+
+JSValueRef CVp(JSContextRef ctx, JSObjectRef function,
+               JSObjectRef thisObject, size_t argumentCount,
+               const JSValueRef arguments[], JSValueRef *exception)
+{
+
+  std::cout << "NZ";
+
+  return JSValueMakeNull(ctx);
+}
 int main()
 {
 
@@ -102,9 +227,8 @@ int main()
   window->SetTitle("TelegramGUI");
 
   app->set_window(window);
-
-  UltralightMain my_app(window);
-  window->set_listener(&my_app);
+  sa.SetWindow(window);
+  window->set_listener(&sa);
   // Start the main loop
   app->Run();
 
